1134c90d531758fdb2c5cff66527b51197df7ed2
Ori Seri <oris@stackpulse.io>
2023-02-20 09:32:09 +0200

    others can be null

--- storage/db/migrations/000003_create_orders_table.up.sql
+++ storage/db/migrations/000003_create_orders_table.up.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS orders (
     venue_name TEXT NOT NULL,
     venue_id TEXT NOT NULL,
     venue_link TEXT NULL,
-    venue_city TEXT NOT NULL,
+    venue_city TEXT NULL,
     host TEXT NOT NULL,
     host_id TEXT NULL,
     status INTEGER NOT NULL,
-    participants JSON NOT NULL,
-    delivery_rate INTEGER NOT NULL
+    participants JSON NULL,
+    delivery_rate INTEGER NULL
 );
